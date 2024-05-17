e6117cf48defcc2e9610bdeb0c8ccbfaf066003f
Davin <167453234+github-cloudlabsuser-755@users.noreply.github.com>
2024-05-17 23:32:32 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,21 +1,35 @@
-//create web server
-var express = require('express');
-var app = express();
+//create web server and listen on port 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-//set the view engine to ejs
-app.set('view engine', 'ejs');
+// import comments.js
+const comments = require('./comments.js');
 
-//use res.render to load up an ejs view file
-
-// index page
-app.get('/', function(req, res) {
-    res.render('pages/index');
+// when user access the root of the website
+app.get('/', (req, res) => {
+    res.send('Welcome to our website!');
 });
 
-// about page
-app.get('/about', function(req, res) {
-    res.render('pages/about');
+// when user access the /comments route
+app.get('/comments', (req, res) => {
+    res.json(comments);
 });
 
-app.listen(8080);
-console.log('8080 is the magic port');
+// when user access the /comments/:id route
+app.get('/comments/:id', (req, res) => {
+    const id = req.params.id;
+    const comment = comments.find((comment) => comment.id === parseInt(id));
+    res.json(comment);
+});
+
+// start the server
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
+
+// Path: comments.js
+//create a list of comments
+module.exports = [
+    {
+        id: 1,
